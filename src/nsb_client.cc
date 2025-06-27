d52c3c6711dc2d68b1f16256b679311501ccb01e
Hari Kuttivelil <harikuttivelil@gmail.com>
2025-06-27 10:34:42 -0700

    Added documentation + staging development for multiple simulator clients.

--- src/nsb_client.cc
+++ src/nsb_client.cc
@@ -208,42 +208,6 @@ namespace nsb {
         comms.sendMessage(nsb::Comms::Channel::SEND, nsbMsg.SerializeAsString());
     }
 
-    /**
-     * @brief Receives a payload via NSB.
-     * 
-     * The implementations of this function differ based on the system mode.
-     *
-     * *In __PULL__ mode:*
-     * If the destination is specified, it will receive a payload for that 
-     * destination. This method creates an NSB RECEIVE message with the 
-     * appropriate information and payload and sends it to the daemon. It will
-     * then get a response that either contains a MESSAGE code and 
-     * carries the retrieved payload or contains a NO_MESSAGE code. If a 
-     * message is found, the entire NSB message is returned to provide access
-     * to the metadata.
-     * 
-     * *In __PUSH__ mode:*
-     * This method will await a message on the Comms.Channels.RECV channel 
-     * using _select_, with an optional timeout. If you want to achieve polling
-     * behavior, set _timeout_ to be 0. If this is being used to listen 
-     * indefinitely, set the timeout to be None. Listening indefinitely 
-     * will result in blocking behavior, but is recommended for asynchronous 
-     * listener implementations.
-     * 
-     * @param destId The identifier of the destination NSB client. The default
-     *               None value will automatically assume the destination is 
-     *               self.
-     * @param timeout The amount of time in seconds to wait to receive data. 
-     *                None denotes waiting indefinitely while 0 denotes polling
-     *                behavior.
-     * 
-     * @returns nsb.nsbm* The NSB message containing the received payload and 
-     *                    metadata if a message is found, otherwise None.
-     * 
-     * @see Config.SystemMode
-     * @see SocketInterface.receiveMessage()
-     * 
-     */
     nsb::nsbm* NSBAppClient::receive(std::string* destId, int timeout) {
         nsb::nsbm* nsbMsg = new nsb::nsbm();
         if (cfg.SYSTEM_MODE == Config::SystemMode::PULL) {
